059add9fc3135389d34486fb9c438b169e8421bd
itorriani <itorrian@calpoly.edu>
2026-01-21 20:52:33 -0800

    fix * used correct read logic

--- read.c
+++ read.c
@@ -1,13 +1,17 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <fcntl.h>   
+#include <unistd.h>  
 #define ARRAY_SIZE 500;    
 
 
 char *readFile(char* fileName, char contents[], int bytes)
 {
-    int file = open(fileName, "r"); //open a file with read
+    int file = open(fileName, O_RDONLY); //open a file with read
 
-    ssize_t bytes_read = read(file, contents, sizeof(contents)-1); // use read system call to read the file
+    ssize_t bytes_read;
+
+    while ((bytes_read=read(file, contents, bytes)) > 0) { }; // use read functionality 
 
     contents[bytes_read] = '\0'; // null terminate the contents
 
