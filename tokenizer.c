f1f04a9fef0de08e69c8da11c3117f653b634e19
itorriani <itorrian@calpoly.edu>
2026-01-24 12:36:41 -0800

    Feat * functionality for stdin tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -7,24 +7,23 @@ int main(int argc, char *argv[1]) // fileName,
 {   
     char contents[CONTENT_SIZE]; //leave plenty of room for contents of a file
 
-    FILE *file = fopen(argv[1], "r"); //read the file
-
-    while (fgets(contents, CONTENT_SIZE, file) != NULL) // read until the last line (when it's null)
+    while (fgets(contents, CONTENT_SIZE, stdin) != NULL) // read until the last line (when it's null)
     {
         contents[strcspn(contents, "\r\n")] = '\0'; // clean up content for new characters
 
-        char *linePtr = strtok(contents, " "); // create a pointer to a tokenized version of each line
+        char *linePtr = strtok(contents, " \t\r\n\v\f"); // create a pointer to a tokenized version of each line
 
         while (linePtr != NULL)
         {
-            printf("%s \n", *linePtr);
+            printf("%s \n", *linePtr); // print each word in the line
 
-
+            strtok(NULL, " \t\r\n\v\f");
 
         }
 
-
     }
+
+    return 0; //return from function
 
 
 }   
